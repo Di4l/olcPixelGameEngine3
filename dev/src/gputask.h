6b41cd2a3f083f43af002f3d44c96e7fb6b50d10
Javidx9 <25419386+OneLoneCoder@users.noreply.github.com>
2025-06-28 21:52:12 +0100

    started work on polyformer. This is where the rabbit hole begins lol...

---
+++ dev/src/gputask.h
@@ -0,0 +1,82 @@
+#pragma once
+
+//! START STDHEADER GLOBAL
+#include <cstdint>
+#include <string>
+#include <array>
+#include <vector>
+//! END STDHEADER
+
+//! START CUSTOMHEADER
+#include "config.h"
+#include "pixel.h"
+//! END CUSTOMHEADER
+
+//! START DECLARATION
+#if !defined(PGE_GPUTASK_DECLARED)
+namespace olc
+{
+	namespace pgeguts
+	{
+		// This is the default "packet" of work that is sent to 
+		// a GPU for drawing. Various drawing operations throughout
+		// PGE create GPUTasks which are stored and dispatched when
+		// appropriate. It's contents are purposefully abstract
+		// to ensure rendering tools do not need to be reliant
+		// upon PGE structures
+		struct GPUTask
+		{
+			struct Vertex 
+			{
+				float p[6];     // x, y, z, w, u, v
+				olc::Pixel c;	// 32-bit colour
+			};
+
+			// Simple vertex buffer
+			std::vector<Vertex> vertexBuffer;
+
+			// Model-View-Projection Matrix for shader
+			std::array<float, 16> mvpMatrix = { {
+				1, 0, 0, 0,
+				0, 1, 0, 0,
+				0, 0, 1, 0,
+				0, 0, 0, 1
+			} };
+
+			// Use depth components
+			bool bDepth = false;
+
+			// Overall biasing colour (great for blends)
+			olc::Pixel tint = olc::Colour::WHITE;
+
+			// Define super structure to be drawn
+			enum class Structure : uint8_t
+			{
+				// Vertex buffer is a series of points	
+				Point = 0, 
+				// Vertex buffer is a series of line segments
+				Line,
+				// Vertex buffer is a fan of triangles
+				Fan,
+				// Vertex buffer is a strip of adjacent triangles
+				Strip,
+				// Vertex buffer is a series of discrete triangles
+				List				
+			} structure = Structure::Fan;
+
+			// Define if GPU should face cull based on winding order
+			enum class CullMode : uint8_t
+			{
+				// No
+				None = 0,
+				// Cull if vertices are listed in clockwise order
+				ClockWise,
+				// Cull if vertices are listed in anticlockwise order
+				CounterClockWise
+			} cullmode = CullMode::None;
+		};
+	}
+}
+#define PGE_GPUTASK_DECLARED 1
+#endif
+//! END DECLARATION
---
+++ dev/src/polyform.h
@@ -0,0 +1,25 @@
+#pragma once
+
+//! START STDHEADER GLOBAL
+#include <cmath>
+#include <cstdint>
+#include <string>
+#include <array>
+//! END STDHEADER
+
+//! START CUSTOMHEADER
+#include "config.h"
+//! END CUSTOMHEADER
+
+//! START DECLARATION
+#if !defined(PGE_POLYFORM_DECLARED)
+namespace olc
+{	
+	namespace polyformer
+	{
+
+	}
+}
+#define PGE_POLYFORM_DECLARED 1
+#endif
+//! END DECLARATION
