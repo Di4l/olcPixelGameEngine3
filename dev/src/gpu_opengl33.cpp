ee45b49150135884c139c8ef9d8bf5ce8d807b3d
Ciaran Moran <iCiaran@users.noreply.github.com>
2026-01-25 01:32:37 +0000

    Correct EGL context version
    EGL_CONTEXT_MAJOR_VERSION is the newer equivalent flag to EGL_CONTEXT_CLIENT_VERSION.
    I think we were getting away with this because of the '-sMIN_WEBGL_VERSION=2' compilation flag,
    however better to be explicit and match the 'version 300 es' in the shaders.
    This change allows running with assertions enabled to debug other issues.

--- dev/src/gpu_opengl33.cpp
+++ dev/src/gpu_opengl33.cpp
@@ -295,7 +295,7 @@ void main()
 #endif
 
 	EGLint const attribute_list[] = {EGL_RED_SIZE, 8, EGL_GREEN_SIZE, 8, EGL_BLUE_SIZE, 8, EGL_ALPHA_SIZE, 8, EGL_DEPTH_SIZE, 16, EGL_SAMPLE_BUFFERS, 1, EGL_SAMPLES, OLC_MSAA_SAMPLES, EGL_NONE};
-	EGLint const context_config[] = {EGL_CONTEXT_CLIENT_VERSION, 2, EGL_NONE};
+	EGLint const context_config[] = {EGL_CONTEXT_MAJOR_VERSION, 3, EGL_NONE};
 	EGLint num_config;
 
 	glRenderContext.display = eglGetDisplay(display);
