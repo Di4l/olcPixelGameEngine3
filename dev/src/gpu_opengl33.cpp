b055025b90d63c23f5a84b7298bfc4eb40f092ed
Ciaran Moran <iCiaran@users.noreply.github.com>
2026-02-28 01:05:10 +0000

    Use wglCreateContextAttribsARB for context creation on windows

--- dev/src/gpu_opengl33.cpp
+++ dev/src/gpu_opengl33.cpp
@@ -238,9 +238,34 @@ void main()
 		}
 
 		auto glDeviceContext = GetDC((HWND)(os_win_id[0]));
+		HGLRC tempContext = wglCreateContext(glDeviceContext);
+		if (!tempContext)
+		{
+			lastError = RendererError::FailedToCreateRenderContext;
+			return false;
+		}
+		wglMakeCurrent(glDeviceContext, tempContext);
+
+		typedef HGLRC(WINAPI* PFN_wglCreateContextAttribsARB)(HDC, HGLRC, const int*);
+		auto pfnCreateContextAttribs = (PFN_wglCreateContextAttribsARB)wglGetProcAddress("wglCreateContextAttribsARB");
+
+		wglMakeCurrent(nullptr, nullptr);
+		wglDeleteContext(tempContext);
+
+		if (pfnCreateContextAttribs)
+		{
+			int gl33_attribs[] = {
+				0x2091, 3,			// WGL_CONTEXT_MAJOR_VERSION_ARB
+				0x2092, 3,			// WGL_CONTEXT_MINOR_VERSION_ARB
+				0x9126, 0x00000001, // WGL_CONTEXT_PROFILE_MASK_ARB = CORE
+				0};
+			glRenderContext = pfnCreateContextAttribs(glDeviceContext, nullptr, gl33_attribs);
+		}
+		else
+			glRenderContext = wglCreateContext(glDeviceContext);
 
 		// Create OpenGL Render Context
-		if (!(glRenderContext = wglCreateContext(glDeviceContext))) 
+		if (!glRenderContext)
 		{
 			lastError = RendererError::FailedToCreateRenderContext;
 			return false;
@@ -251,6 +276,7 @@ void main()
 			lastError = RendererError::FailedToSwitchRenderContext;
 			return false;
 		}
+
 #endif
 
 #if OLC_HOST == OLC_HOST_LINUX_X11
