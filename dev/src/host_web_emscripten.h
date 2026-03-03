c11f25c6623d4fbc5f881709b9883472e5981189
Moros Smith <moros1138@gmail.com>
2026-03-02 19:56:11 -0500

    [emscripten] add SetFullscreen to host

--- dev/src/host_web_emscripten.h
+++ dev/src/host_web_emscripten.h
@@ -43,6 +43,7 @@ namespace olc::host
         bool SetMousePosition(olc::Window* pWindow, const olc::vi2d& vPos) override;
         // Show or hide mouse cursor for given window
         bool SetMouseVisible(olc::Window* pWindow, const bool bVisible) override;
+        bool SetFullScreen(olc::Window* pWindow, const bool bFullScreen) override;
 
     public: // OS Specific Environment Information
         olc::KeyboardLayout GetKeyboardLayout() const override;
